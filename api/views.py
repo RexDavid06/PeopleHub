9eb88769952e174d297ec85df65a324cafd11123
Buchi Rex-David <rhexmilia06@gmail.com>
2026-04-02 12:22:53 +0100

    converting serialized data to JSON, then rednder the JSON_DATA to be sent to the frontend

--- api/views.py
+++ api/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from .models import Person
 from  .serializers import PersonSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
 # Create your views here.
 def singleobj(request):
     data = Person.objects.get(id=1)
     serializer = PersonSerializer(data)
-    print(serializer.data)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data,  content_type='application/json')
+
+
 
 def multipleobj(request):
     data = Person.objects.all()
     serializer   = PersonSerializer(data, many=True)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data,   content_type='application/json')
     print(serializer.data)
 
     
